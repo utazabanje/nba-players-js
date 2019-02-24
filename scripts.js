5838f7e09607c1e66de3dc5886e9f65f38b08747
Miodrag Piletic <utazabanje@hotmail.com>
2019-02-24 12:03:59 +0100

    Refactor create player function

--- scripts.js
+++ scripts.js
@@ -1,9 +1,6 @@
-let playersRow = document.querySelector('.players-row');
-let playersCol = document.querySelector('.players-col');
 let url = "http://www.json-generator.com/api/json/get/cfmbOlMLGW?indent=2";
 let allPlayersArray = [];
-let singlePlayerArray = '';
-let randomPlayer = null;
+let playerIndex = [];
 
 (function() {
     fetch(url)
@@ -11,33 +8,29 @@ let randomPlayer = null;
         .then((data) => {
             allPlayersArray = data.league.standard;
 
-            allPlayersArray.forEach(element => {
-                singlePlayerArray = element.firstName + ' ' + element.lastName;
-            });
+            //get one random player
+            let randomPlayerIndex = Math.floor(allPlayersArray.length * Math.random());
+            // get 10 random players from allplayers array with splice method
+            let randomPlayerSplice = allPlayersArray.splice(randomPlayerIndex, 10);
+
+            //create 10 players with createPlayer function. Argument is single array 
+            for(let i = 0; i < 10; i++) {
+                randomPlayerSplice[i];
+                console.log(randomPlayerSplice[i]);
+
+                createPlayer(randomPlayerSplice[i]);
+            }
         })
         .catch((error) => console.log(error));
 })();
 
-// function for calling tenRandomPlayers because data from json is not loading instantly
-// for loop is for 10 players because tenRandomPlayers function is producing only one player
-setTimeout(function() {
-    for(let i = 0; i < 10; i++) {
-        createPlayerGrid(tenRandomPlayers());
-    }
-}, 1000);
+// function that creates single player
+function createPlayer(singlePlayer) {
+    let playerHtml = document.getElementById('singlePlayer').cloneNode(true);
+    playerHtml.removeAttribute('id');
 
-// making random player from allPlayers array using math floor 
-// variable randomPlayer contains all info about player (randomPlayer.firstName) would give players first name
-function tenRandomPlayers() {
-    randomPlayer = allPlayersArray[Math.floor(allPlayersArray.length * Math.random())];
-    console.log(randomPlayer.firstName)
-}
+    playerHtml.classList.add('col-6');
+    playerHtml.querySelector('.player-name').innerHTML = singlePlayer.firstName + ' ' + singlePlayer.lastName;
 
-// creating grid for loaded players
-function createPlayerGrid(playerGrid) {
-    let playersCol = document.createElement('div');
-    playersCol.classList = "col-6 players-col";
-    playersCol.innerHTML = randomPlayer.firstName + ' ' + randomPlayer.lastName;
-
-    playersRow.appendChild(playersCol);
+    document.getElementById('singlePlayerGrid').appendChild(playerHtml);
 }
